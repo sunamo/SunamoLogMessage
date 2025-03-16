25a29ed08dca5861b6809f39634d78bd62327c72
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-16 14:30:17 +0100

    Changes due to creating SunamoWpf

---
+++ WpfControlGenerator.cs
@@ -0,0 +1,35 @@
+namespace desktop;
+
+public class WpfControlGenerator
+{
+    public static StackPanel VerticalColoredList(List<ILogMessage<Color, string>> c)
+    {
+        StackPanel sp = new StackPanel();
+        sp.Orientation = Orientation.Vertical;
+        foreach (var item in c)
+        {
+            Grid g = new Grid();
+            g.Background = new SolidColorBrush(item.Bg);
+            TextBlock tb = new TextBlock();
+            tb.Text = item.Message;
+            Grid.SetColumn(tb, 0);
+            Grid.SetRow(tb, 0);
+            g.Children.Add(tb);
+            sp.Children.Add(g);
+        }
+        return sp;
+    }
+
+    public static Grid LogMessage(ILogMessage<Color, string> c)
+    {
+        Grid g = new Grid();
+        g.Background = new SolidColorBrush(c.Bg);
+        TextBlock tb = new TextBlock();
+        tb.Text = c.Message;
+        tb.TextWrapping = TextWrapping.Wrap;
+        Grid.SetColumn(tb, 0);
+        Grid.SetRow(tb, 0);
+        g.Children.Add(tb);
+        return g;
+    }
+}
